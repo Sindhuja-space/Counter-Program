108c3c969390146f042edff5316c0e005132c26a
Sindhuja-space <sindhujapvt@gmail.com>
2024-05-04 20:05:51 +0530

    modified files

---
+++ script.js
@@ -0,0 +1,19 @@
+const decreaseBtn = document.getElementById("decreaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const increaseBtn = document.getElementById("increaseBtn")
+const result = document.getElementById("result")
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    result.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+   result.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    result.textContent = count;
+}
+
